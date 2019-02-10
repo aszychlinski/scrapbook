e195169dabaf2541d69ae62b8dc65a7350baa354
aszychlinski <36963999+aszychlinski@users.noreply.github.com>
2019-02-10 20:20:27 +0100

    added if __name__ == '__main__': to RPS

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,146 +3,145 @@ import threading as th
 from random import choice
 from time import sleep
 
-root = tk.Tk()
-root.geometry('300x325')
-root.title('Rock-Paper-Scissors')
-root.resizable(False, False)
-DEFAULT_BG = root.cget('bg')
 
-ROCK = '\u2BC2'
-PAPER = '2'
-SCISSORS = '\u2700'
+def main():
+    root = tk.Tk()
+    root.geometry('300x325')
+    root.title('Rock-Paper-Scissors')
+    root.resizable(False, False)
+    default_bg = root.cget('bg')
 
+    rock = '\u2BC2'
+    paper = '2'
+    scissors = '\u2700'
 
-class RPSButton:
-    def __init__(self, master, **kwargs):
-        self.button = tk.Button(master, **kwargs)
-        self.content = self.button['text']
-        self.button['command'] = self.push_update
+    class RPSButton:
+        def __init__(self, master, **kwargs):
+            self.button = tk.Button(master, **kwargs)
+            self.content = self.button['text']
+            self.button['command'] = self.push_update
 
-    def push_update(self):
-        p1_content.set(self.content)
+        def push_update(self):
+            p1_content.set(self.content)
 
+    class RPSSpinner(th.Thread):
+        def __init__(self):
+            th.Thread.__init__(self)
+            self.kill = False
 
-class RPSSpinner(th.Thread):
-    def __init__(self):
-        th.Thread.__init__(self)
-        self.kill = False
+        def run(self):
+            while not self.kill:
+                cpu_content.set(choice([rock, paper, scissors]))
+                sleep(0.05)
+            return None
 
-    def run(self):
-        while not self.kill:
-            cpu_content.set(choice([ROCK, PAPER, SCISSORS]))
-            sleep(0.05)
-        return None
+    class NotGlobalHehe:
+        cpu_logic = RPSSpinner()
+        player_wins = 0
+        cpu_wins = 0
 
+    def resolve_round():
+        if not p1_content.get():
+            pass
+        else:
+            go_button.config(state='disabled')
+            [x.button.config(state='disabled') for x in (p1_r, p1_p, p1_s)]
+            NotGlobalHehe.cpu_logic.kill = True
+            sleep(0.1)
+            p1, cpu = p1_content.get(), cpu_content.get()
+            if p1 == cpu:
+                round_result.config(text='DRAW', bg='yellow')
+            elif (p1 == rock and cpu == scissors) or (p1 == paper and cpu == rock) or (p1 == scissors and cpu == paper):
+                round_result.config(text='PLAYER WINS', bg='green')
+                NotGlobalHehe.player_wins += 1
+            elif (p1 == rock and cpu == paper) or (p1 == paper and cpu == scissors) or (p1 == scissors and cpu == rock):
+                round_result.config(text='CPU WINS', bg='red')
+                NotGlobalHehe.cpu_wins += 1
+            score.config(text=f'{NotGlobalHehe.player_wins}:{NotGlobalHehe.cpu_wins}')
+            restart_question.config(text='Play again?')
+            restart_btn.config(state='normal', relief='raised', bg='green', text='YES')
+            quit_btn.config(state='normal', relief='raised', bg='red', text='NO')
 
-class NotGlobalHehe:
-    cpu_logic = RPSSpinner()
-    player_wins = 0
-    cpu_wins = 0
-
-
-def resolve_round():
-    if not p1_content.get():
-        pass
-    else:
-        go_button.config(state='disabled')
-        [x.button.config(state='disabled') for x in (p1_r, p1_p, p1_s)]
-        NotGlobalHehe.cpu_logic.kill = True
+    def restart_round():
+        NotGlobalHehe.cpu_logic = RPSSpinner()
+        NotGlobalHehe.cpu_logic.start()
         sleep(0.1)
-        p1, cpu = p1_content.get(), cpu_content.get()
-        if p1 == cpu:
-            round_result.config(text='DRAW', bg='yellow')
-        elif (p1 == ROCK and cpu == SCISSORS) or (p1 == PAPER and cpu == ROCK) or (p1 == SCISSORS and cpu == PAPER):
-            round_result.config(text='PLAYER WINS', bg='green')
-            NotGlobalHehe.player_wins += 1
-        elif (p1 == ROCK and cpu == PAPER) or (p1 == PAPER and cpu == SCISSORS) or (p1 == SCISSORS and cpu == ROCK):
-            round_result.config(text='CPU WINS', bg='red')
-            NotGlobalHehe.cpu_wins += 1
-        score.config(text=f'{NotGlobalHehe.player_wins}:{NotGlobalHehe.cpu_wins}')
-        restart_question.config(text='Play again?')
-        restart_btn.config(state='normal', relief='raised', bg='green', text='YES')
-        quit_btn.config(state='normal', relief='raised', bg='red', text='NO')
+        go_button.config(state='normal')
+        [x.button.config(state='normal') for x in (p1_r, p1_p, p1_s)]
+        p1_choice.config(text='')
+        restart_question.config(text=' ')
+        restart_btn.config(state='disabled', relief='flat', bg=default_bg, text='')
+        quit_btn.config(state='disabled', relief='flat', bg=default_bg, text='')
 
+    top_frame, mid_frame, score_frame, bot_frame = tk.Frame(root), tk.Frame(root), tk.Frame(root), tk.Frame(root)
+    [x.pack(side='top', fill='x') for x in (top_frame, mid_frame, score_frame, bot_frame)]
 
-def restart_round():
-    NotGlobalHehe.cpu_logic = RPSSpinner()
+    # --- top_frame ---
+    info_label = tk.Label(top_frame, text='Pick a symbol and click "Go!" to resolve a round.')
+    info_label.pack(side='top', pady=5)
+
+    # --- mid_frame ---
+    p1_frame, versus_frame, cpu_frame = tk.Frame(mid_frame), tk.Label(mid_frame), tk.Label(mid_frame)
+    [x.pack(side='top', fill='x') for x in (p1_frame, versus_frame, cpu_frame)]
+
+    p1_content = tk.StringVar()
+    cpu_content = tk.StringVar()
+
+    p1_r = RPSButton(p1_frame, font='Wingdings', text=rock, width=3)
+    p1_p = RPSButton(p1_frame, font='Wingdings', text=paper, width=3)
+    p1_s = RPSButton(p1_frame, font='Wingdings', text=scissors, width=3)
+
+    p1_r.button.pack(side='left', padx=5)
+    p1_p.button.pack(side='left')
+    p1_s.button.pack(side='left', padx=5)
+
+    p1_choice = tk.Button(versus_frame, font='Wingdings', relief='groove', textvariable=p1_content, width=2)
+    go_button = tk.Button(versus_frame, text='Go!', command=resolve_round)
+    cpu_choice = tk.Button(versus_frame, font='Wingdings', relief='groove', textvariable=cpu_content, width=2)
+
+    p1_choice.pack(side='left', padx=45, pady=5)
+    go_button.pack(side='left', pady=5)
+    cpu_choice.pack(side='left', padx=45, pady=5)
+
     NotGlobalHehe.cpu_logic.start()
-    sleep(0.1)
-    go_button.config(state='normal')
-    [x.button.config(state='normal') for x in (p1_r, p1_p, p1_s)]
-    p1_choice.config(text='')
-    restart_question.config(text=' ')
-    restart_btn.config(state='disabled', relief='flat', bg=DEFAULT_BG, text='')
-    quit_btn.config(state='disabled', relief='flat', bg=DEFAULT_BG, text='')
+
+    cpu_r = RPSButton(cpu_frame, font='Wingdings', text='\u2BC2', width=3, state='disabled', relief='sunken')
+    cpu_p = RPSButton(cpu_frame, font='Wingdings', text='2', width=3, state='disabled', relief='sunken')
+    cpu_s = RPSButton(cpu_frame, font='Wingdings', text='\u2700', width=3, state='disabled', relief='sunken')
+
+    cpu_r.button.pack(side='right', padx=5)
+    cpu_p.button.pack(side='right')
+    cpu_s.button.pack(side='right', padx=5)
+
+    # --- score_frame ---
+    round_result = tk.Button(score_frame, relief='groove')
+    round_result.pack(side='top', fill='x', pady=5)
+
+    score_intro = tk.Label(score_frame, text='Current score:')
+    score_intro.pack(side='top')
+
+    player_label = tk.Label(score_frame, text='Player →', anchor='e')
+    player_label.pack(side='left', fill='x', expand=1)
+
+    score = tk.Button(score_frame,
+                      text=f'{NotGlobalHehe.player_wins}:{NotGlobalHehe.cpu_wins}',
+                      relief='groove',
+                      width=7,
+                      state='disabled')
+    score.pack(side='left')
+
+    cpu_label = tk.Label(score_frame, text='← CPU', anchor='w')
+    cpu_label.pack(side='left', fill='x', expand=1)
+
+    # --- bot_frame ---
+    restart_question = tk.Label(bot_frame, text=' ')
+    restart_question.pack(side='top')
+    restart_btn = tk.Button(bot_frame, height=5, width=15, state='disabled', relief='flat', command=restart_round)
+    quit_btn = tk.Button(bot_frame, height=5, width=15, state='disabled', relief='flat', command=exit)
+    restart_btn.pack(side='left', expand=1, pady=5), quit_btn.pack(side='left', expand=1, pady=5)
+
+    root.mainloop()
 
 
-top_frame, mid_frame, score_frame, bot_frame = tk.Frame(root), tk.Frame(root), tk.Frame(root), tk.Frame(root)
-[x.pack(side='top', fill='x') for x in (top_frame, mid_frame, score_frame, bot_frame)]
-
-# --- top_frame ---
-info_label = tk.Label(top_frame, text='Pick a symbol and click "Go!" to resolve a round.')
-info_label.pack(side='top', pady=5)
-
-# --- mid_frame ---
-p1_frame, versus_frame, cpu_frame = tk.Frame(mid_frame), tk.Label(mid_frame), tk.Label(mid_frame)
-[x.pack(side='top', fill='x') for x in (p1_frame, versus_frame, cpu_frame)]
-
-p1_content = tk.StringVar()
-cpu_content = tk.StringVar()
-
-p1_r = RPSButton(p1_frame, font='Wingdings', text=ROCK, width=3)
-p1_p = RPSButton(p1_frame, font='Wingdings', text=PAPER, width=3)
-p1_s = RPSButton(p1_frame, font='Wingdings', text=SCISSORS, width=3)
-
-p1_r.button.pack(side='left', padx=5)
-p1_p.button.pack(side='left')
-p1_s.button.pack(side='left', padx=5)
-
-p1_choice = tk.Button(versus_frame, font='Wingdings', relief='groove', textvariable=p1_content, width=2)
-go_button = tk.Button(versus_frame, text='Go!', command=resolve_round)
-cpu_choice = tk.Button(versus_frame, font='Wingdings', relief='groove', textvariable=cpu_content, width=2)
-
-p1_choice.pack(side='left', padx=45, pady=5)
-go_button.pack(side='left', pady=5)
-cpu_choice.pack(side='left', padx=45, pady=5)
-
-NotGlobalHehe.cpu_logic.start()
-
-cpu_r = RPSButton(cpu_frame, font='Wingdings', text='\u2BC2', width=3, state='disabled', relief='sunken')
-cpu_p = RPSButton(cpu_frame, font='Wingdings', text='2', width=3, state='disabled', relief='sunken')
-cpu_s = RPSButton(cpu_frame, font='Wingdings', text='\u2700', width=3, state='disabled', relief='sunken')
-
-cpu_r.button.pack(side='right', padx=5)
-cpu_p.button.pack(side='right')
-cpu_s.button.pack(side='right', padx=5)
-
-# --- score_frame ---
-round_result = tk.Button(score_frame, relief='groove')
-round_result.pack(side='top', fill='x', pady=5)
-
-score_intro = tk.Label(score_frame, text='Current score:')
-score_intro.pack(side='top')
-
-player_label = tk.Label(score_frame, text='Player →', anchor='e')
-player_label.pack(side='left', fill='x', expand=1)
-
-score = tk.Button(score_frame,
-                  text=f'{NotGlobalHehe.player_wins}:{NotGlobalHehe.cpu_wins}',
-                  relief='groove',
-                  width=7,
-                  state='disabled')
-score.pack(side='left')
-
-cpu_label = tk.Label(score_frame, text='← CPU', anchor='w')
-cpu_label.pack(side='left', fill='x', expand=1)
-
-# --- bot_frame ---
-restart_question = tk.Label(bot_frame, text=' ')
-restart_question.pack(side='top')
-restart_btn = tk.Button(bot_frame, height=5, width=15, state='disabled', relief='flat', command=restart_round)
-quit_btn = tk.Button(bot_frame, height=5, width=15, state='disabled', relief='flat', command=exit)
-restart_btn.pack(side='left', expand=1, pady=5), quit_btn.pack(side='left', expand=1, pady=5)
-
-
-root.mainloop()
+if __name__ == '__main__':
+    main()
